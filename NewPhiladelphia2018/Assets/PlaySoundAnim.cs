3af632738df61f0ab1c8003cd58d9c708f124b18
Ryan Hallisey <rhallis1@student.fitchburgstate.edu>
2019-01-06 11:36:51 -0500

    Adding Sounds Back

---
+++ NewPhiladelphia2018/Assets/PlaySoundAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundAnim : MonoBehaviour {
+
+    AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        audio.PlayOneShot(audio.clip);
+
+    }
+
+}
